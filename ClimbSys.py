875f6723a7e5f30e571a7f087fd277c526083af1
Clara Arandiga <claraarandiga@gmail.com>
2024-07-26 14:50:41 -0300

    Fixed ClimbSys

--- ClimbSys.py
+++ ClimbSys.py
@@ -11,14 +11,14 @@ class ClimbSys:
         self.climb_motor_right = phoenix5.WPI_VictorSPX(3) 
 
     def teleop(self):
-        if self.joystick.getRawButton(4):
-            self.climb_motor_left.set(1.0) 
+        if self.joystick.getRawButton(4): # While pressed -> motor up
+            self.climb_motor_left.set(1.0)  
             self.climb_motor_right.set(1.0) 
 
-        elif self.joystick.getRawButton(2): 
+        elif self.joystick.getRawButton(2): # While pressed -> motor down
             self.climb_motor_left.set(-1.0) 
             self.climb_motor_right.set(-1.0) 
 
-        else:
+        else: # -> motor off
             self.climb_motor_left.set(0.0) 
             self.climb_motor_right.set(0.0)
