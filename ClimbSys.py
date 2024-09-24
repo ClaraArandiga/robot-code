42eff8ea74d2f93b220a61d2489be47fbf5503b3
Clara Arandiga <claraarandiga@gmail.com>
2024-09-24 19:43:35 -0300

    Updated Code

--- ClimbSys.py
+++ ClimbSys.py
@@ -1,18 +1,16 @@
 import wpilib
-import wpilib.drive
 import phoenix5 
-import rev    
 
-class elevationSys:
-
-    def init(self):
+class ElevationSys:
+    def __init__(self):
         self.joystick = wpilib.Joystick(0)
-        self.climb_motor_left = phoenix5.WPI_VictorSPX(2) 
+        self.climb_motor_left = phoenix5.WPI_VictorSPX(9) 
         self.climb_motor_right = phoenix5.WPI_VictorSPX(8)
+        self.climb_motor_left.setInverted(True)
 
     def teleop(self):
         if self.joystick.getRawButton(4): # While pressed -> motor up
-            self.climb_motor_left.set(1.0)  
+            self.climb_motor_left.set(1.0)
             self.climb_motor_right.set(1.0) 
 
         elif self.joystick.getRawButton(2): # While pressed -> motor down
--- robot.py
+++ robot.py
@@ -1,41 +1,53 @@
 import wpilib
 import wpilib.drive
-import phoenix5 
-from ClimbSys import elevationSys
-import rev
+import rev 
+from ClimbSys import ElevationSys
 
 class TestRobot(wpilib.TimedRobot):
     def robotInit(self):
-        self.left_front_motor = rev.CANSparkMax(10, rev.CANSparkLowLevel.MotorType.kBrushless)
-        self.left_rear_motor = rev.CANSparkMax(51, rev.CANSparkLowLevel.MotorType.kBrushless)
-        self.right_front_motor = rev.CANSparkMax(8, rev.CANSparkLowLevel.MotorType.kBrushless)
-        self.right_rear_motor = rev.CANSparkMax(7, rev.CANSparkLowLevel.MotorType.kBrushless)
+        # Initialize motors using CAN IDs
+        self.left_front_motor = rev.CANSparkMax(50, rev.CANSparkLowLevel.MotorType.kBrushed)
+        self.left_rear_motor = rev.CANSparkMax(51, rev.CANSparkLowLevel.MotorType.kBrushed)
+        self.right_front_motor = rev.CANSparkMax(53, rev.CANSparkLowLevel.MotorType.kBrushed)
+        self.right_rear_motor = rev.CANSparkMax(52, rev.CANSparkLowLevel.MotorType.kBrushed)
 
+        # Group left and right motors
         self.left = wpilib.MotorControllerGroup(self.left_front_motor, self.left_rear_motor)
         self.right = wpilib.MotorControllerGroup(self.right_front_motor, self.right_rear_motor)
 
-        self.motors = wpilib.MotorControllerGroup(self.left, self.right)
+        self.right.setInverted(True)
 
-        self.robot_drive = wpilib.drive.DifferentialDrive(
-            self.left, self.right
-        )
+        # Set up differential drive for arcade driving
+        self.robot_drive = wpilib.drive.DifferentialDrive(self.left, self.right)
 
-        elevationSys.init(self)
-
+        # Initialize joystick for controlling the robot
         self.joystick = wpilib.Joystick(0)
-    
-    def teleopPeriodic(self):
-        move_value = self.joystick.getRawAxis(1)
-        rotate_value = self.joystick.getRawAxis(0)
-        self.robot_drive.arcadeDrive(move_value, rotate_value)
 
-        elevationSys.teleop(self)
+        self.elevation = ElevationSys()
 
     def autonomousInit(self):
         pass
 
     def autonomousPeriodic(self):
-        pass 
+        pass
 
-# if _name_ == "_main_":
-    # wpilib.run(TestRobot)
+    def teleopInit(self):
+        self.robot_drive.setExpiration(0.1)
+        self.robot_drive.setSafetyEnabled(True)
+
+    def axis(self):
+        if self.joystick.getRawAxis(3) > 0:
+            return -self.joystick.getRawAxis(3)
+        elif self.joystick.getRawAxis(4) > 0:
+            return self.joystick.getRawAxis(4)
+        return 0      
+
+    def teleopPeriodic(self):
+        # Get joystick axis values for movement and rotati on
+        move_value = self.axis()  # Y-axis
+        rotate_value = self.joystick.getRawAxis(0)  # X-axis
+
+        # Use arcade drive to move the robot
+        self.robot_drive.arcadeDrive(move_value, rotate_value)
+
+        self.elevation.teleop()
